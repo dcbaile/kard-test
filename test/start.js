7ea2678dc34e3c4f7bbe9a4d86aa73b2df30012d
dylan <dylancbailey@outlook.com>
2022-05-09 14:25:39 -0400

    Got the general structure of the program working. Need to implement asynchronous way to parse strings and write to files so I'm not blockin the thread so heavily

--- test/start.js
+++ test/start.js
@@ -1,46 +1,112 @@
+//Please write your code below this line.
 'use strict';
-import transactions from './transactions.json';
 
 const fs = require('fs');
 const REFERRING_PARTNER_ACCOUNT_ID = "referringPartnerAccountId"
 
-const KARD = "Kard"
-const VERSION = "v1.0"
-const STARTFEED = "STARTFEED"
-const ENDFEED = "ENDFEED"
+const TRANSACTION_RESULTS_PATH = 'test/transactions_result.txt'
+const TRANSACTIONS_JSON_PATH = 'test/transactions.json'
 
 
-function getTransactionJsonObjectList(){
-	let transactionJson = fs.readFileSync('test/transactions.json');
-	return JSON.parse(transactionJson).data
+function getTransactionJsonObjectList() {
+    let transactionJson = fs.readFileSync(TRANSACTIONS_JSON_PATH);
+    return JSON.parse(transactionJson).data
 }
 
-function transactionListToPartnerAccountMap(transactionList){
-	let transactionMap = new Map()
+function transactionListToPartnerAccountMap(transactionList) {
+    let transactionMap = new Map()
 
-	for(let transaction of transactionList){
-		let referringPartnerAccountId = transaction['referringPartnerAccountId'];
-		transactionMap[referringPartnerAccountId] = transactionMap[referringPartnerAccountId] || transactionMap.set(referringPartnerAccountId, []);
-		transactionMap.get(referringPartnerAccountId).push(transaction)
-	}
-	return transactionMap;
+    for (let transaction of transactionList) {
+        let referringPartnerAccountId = transaction['referringPartnerAccountId'];
+        transactionMap[referringPartnerAccountId] = transactionMap[referringPartnerAccountId] || transactionMap.set(referringPartnerAccountId, []);
+        transactionMap.get(referringPartnerAccountId).push(transaction)
+    }
+    return transactionMap;
 }
 
-function calculateTotalLineNumber(transactionMap){
-	let totalCount = 0
-	totalCount += transactionMap.size
-	for(let [accountNumber, transactions] of transactionMap){
-		totalCount += transactions.length
-	}
-	return totalCount
+function lineNumberTotal(transactionMap) {
+    let totalCount = 0
+    totalCount += transactionMap.size
+    for (let [accountNumber, transactions] of transactionMap) {
+        totalCount += transactions.length
+    }
+    return totalCount
 }
 
-//Please write your code below this line.
-export default function() {
+function settledCommissionSum(transactionList) {
+    let settledCommissionSum = 0;
+    for (let transaction of transactionList) {
+        settledCommissionSum += (transaction.status === "SETTLED") ? transaction.commissionToUserInCents : 0
+    }
+    return settledCommissionSum;
+}
 
-	let transactionList = getTransactionJsonObjectList();
+function generateHeader(lineNumberTotal) {
+    let currentTimeStamp = new Date()
+    return `V1.0|STARTFEED|Kard|${currentTimeStamp.toISOString()}|${lineNumberTotal}\n`
+}
 
-	let transactionMap = transactionListToPartnerAccountMap(transactionList);
-	console.log(`number of transactions in this: ${transactionList.length}`)
-	console.log(calculateTotalLineNumber(transactionMap))
+function generateFooter(totalCommission, totalTransactionAmount){
+    return `ENDFEED|${totalCommission}|${totalTransactionAmount}`
+}
+
+function generatePartnerAccountHeader(accountNumber, transactions) {
+    return `|${accountNumber}||AG|||${settledCommissionSum(transactions)}||\n`
+}
+
+function generateTransactionLine(transaction) {
+    let id = transaction._id;
+    let referringPartnerAccountId = transaction.referringPartnerAccountId;
+    let offerId = transaction.offerId;
+    let status = transaction.status;
+    let transactionDate = transaction.transactionDate;
+    let merchantName = transaction.merchantName;
+    let commissionToUserInCents = transaction.commissionToUserInCents;
+    let transactionAmountInCents = transaction.transactionAmountInCents;
+    let source = transaction.source
+
+    return `${id}|${referringPartnerAccountId}|${offerId}|${status}|${transactionDate}|${merchantName}|${commissionToUserInCents}|${transactionAmountInCents}|${source}`
+}
+
+
+function processTransactionsToTxtFile(transactionMap) {
+    let [totalUserCommissionSum, totalTransactionAmountInCents] = [0,0];
+    let lineIndex = 1
+
+    fs.writeFileSync(TRANSACTION_RESULTS_PATH, generateHeader(lineNumberTotal(transactionMap)), (error) => {
+        if (error) {
+            console.log('something bad happened')
+        }
+    })
+
+    for (let [accountNumber, transactions] of transactionMap) {
+        let accountHeader = generatePartnerAccountHeader(accountNumber, transactions)
+        fs.appendFileSync(TRANSACTION_RESULTS_PATH, `${lineIndex++}|${accountHeader}`, (error) => {
+            if (error) {
+                console.log('file Write failed')
+            }
+        })
+
+        for (let transaction of transactions) {
+            totalUserCommissionSum += transaction.commissionToUserInCents
+            totalTransactionAmountInCents += transaction.transactionAmountInCents
+            fs.appendFileSync(TRANSACTION_RESULTS_PATH, `${lineIndex++}|${generateTransactionLine(transaction)}\n`)
+        }
+    }
+    fs.appendFileSync(TRANSACTION_RESULTS_PATH, generateFooter(totalUserCommissionSum, totalTransactionAmountInCents),(error) => {
+        if (error) {
+            console.log('something bad happened')
+        }
+    })
+}
+
+export default function () {
+
+    let transactionList = getTransactionJsonObjectList();
+
+    let transactionMap = transactionListToPartnerAccountMap(transactionList);
+    /*
+        for(let[accountNumber, transactions] of transactionMap){
+        }*/
+    processTransactionsToTxtFile(transactionMap)
 }
